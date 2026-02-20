3d0a01dbc07a97f92045ca91f153b3a11338d044
Vimpleks <evgenya_2005@mail.ru>
2026-02-20 17:35:37 +0700

    Edit tasks module 8.1. 20.02.2026

--- Tasks_Module_81.py
+++ Tasks_Module_81.py
@@ -10,24 +10,36 @@ class Friends:
                 print('Связь должна быть только из двух имен')
 
     def add(self, connection: set[str]):
+        """
+        Добавляет связь друзей в объект
+        """
         if connection in self.connections:
             return False
         self.connections.append(connection)
         return True
 
     def remove(self, connection: set[str]):
+        """
+        Удаляет связь друзей из объекта
+        """
         if connection not in self.connections:
             return False
         self.connections.remove(connection)
         return True
 
     def names(self):
+        """
+        Возвращает множество имён. Множество содержит имена, которые имеют хотя бы одну связь.
+        """
         result = set()
         for connection in self.connections:
             result.update(connection)
         return result
 
     def connected(self, name: str):
+        """
+        Возвращает множество имён, которые связаны с именем, заданным параметром "name"
+        """
         result = set()
         for connection in self.connections:
             if name in connection:
@@ -63,10 +75,16 @@ class Student:
         self.grade_subject = dict()
 
     def add_grade(self,subject: str, grade: int):
+        """
+        Добавляет оценку по предмету
+        """
         self.grade_subject[subject] = grade
         return f'Студенту {self.name_student} добавлена оценка {grade} по предмету {subject}'
 
     def get_avg_grade_student(self):
+        """
+        Возвращает среднюю оценку по всем предметам студента
+        """
         if len(self.grade_subject) == 0:
             return 0
         return round(sum(self.grade_subject.values()) / len(self.grade_subject), 1)
@@ -80,14 +98,23 @@ class GroupStudy:
         self.students = list()
 
     def add_student(self, student: Student):
+        """
+        Добавляет студента в группу
+        """
         self.students.append(student)
         return f'Студент {student.name_student} добавлен в группу {self.number_group}'
 
     def remove_student(self, student: Student):
+        """
+        Удаляет студента из группы
+        """
         self.students.remove(student)
         return f'Студент {student.name_student} удален из группы {self.number_group}'
 
     def get_avg_grade_group(self):
+        """
+        Возвращает среднюю оценку студентов в группе
+        """
         total_avg_grade_students  = 0
         if len(self.students) == 0:
             return 0
@@ -96,6 +123,9 @@ class GroupStudy:
         return round(total_avg_grade_students / len(self.students), 1)
 
     def get_avg_grade_subject(self, subject: str):
+        """
+        Возвращает среднюю оценку по предмету в группе
+        """
         grade_subject_group = list()
         for student in self.students:
             grade_subject_group.append(student.grade_subject[subject])
@@ -110,9 +140,15 @@ class DeanOffice:
         self.groups = list()
 
     def add_group(self, group: GroupStudy):
+        """
+        Добавляет группу на факультет
+        """
         self.groups.append(group)
 
     def get_students_for_expulsion(self, min_avg: float = 2.5):
+        """
+        Возвращает список студентов на отчисление
+        """
         students = list()
         for group in self.groups:
             for student in group.students:
@@ -121,6 +157,9 @@ class DeanOffice:
         return students
 
     def get_scholarship_students(self, max_avg: float = 4.0):
+        """
+        Возвращает список студентов-стипендиатов
+        """
         students = list()
         for group in self.groups:
             for student in group.students:
@@ -129,6 +168,9 @@ class DeanOffice:
         return students
 
     def get_subject_statistics(self):
+        """
+        Вовзращает среднюю оценку по каждому предмету на факультете
+        """
         subject_statistics = dict()
         all_subjects = set()
         for group in self.groups:
@@ -136,9 +178,12 @@ class DeanOffice:
                 for subject in student.grade_subject.keys():
                     all_subjects.add(subject)
             for subject in all_subjects:
-                subject_statistics[subject] = group.get_avg_grade_subject(subject)
+                if subject not in subject_statistics:
+                    subject_statistics[subject] = []
+                subject_statistics[subject].append(group.get_avg_grade_subject(subject))
+        for subject, grades in subject_statistics.items():
+            subject_statistics[subject] = sum(grades) / len(grades)
         return subject_statistics
-
 
 ivanov = Student(1,'Иванов Иван Иванович')
 ivanov.add_grade('Мат. анализ', 5)
@@ -161,19 +206,21 @@ mihailov.add_grade('Теория вероятности', 2)
 mihailov.add_grade('Программирование', 3)
 
 ab520 = GroupStudy('АБ-520')
-ab520.add_student(nikolaev)
+ab521 = GroupStudy('АБ-521')
+ab521.add_student(nikolaev)
 ab520.add_student(petrov)
 ab520.add_student(ivanov)
 ab520.add_student(mihailov)
 
 avt_faculty = DeanOffice('Факультет АВТ')
 avt_faculty.add_group(ab520)
+avt_faculty.add_group(ab521)
 
-#print(f'Средняя оценка у студента {mihailov.name_student} = {mihailov.get_avg_grade_student()}')
-# print(f'Средняя оценка у студентов группы {ab520.number_group} = {ab520.get_avg_grade_group()}')
+# print(f'Средняя оценка студента {mihailov.name_student} = {mihailov.get_avg_grade_student()}')
+# print(f'Средняя оценка студентов группы {ab520.number_group} = {ab520.get_avg_grade_group()}')
 # print(f'Средняя оценка по программированию у студентов группы {ab520.number_group}\
 #  = {ab520.get_avg_grade_subject('Программирование')}')
-#print(f'Студенты на отчисление: {avt_faculty.get_students_for_expulsion()}')
-#print(f'Студенты-стипендиаты: {avt_faculty.get_scholarship_students()}')
-print(avt_faculty.get_subject_statistics())
+# print(f'Студенты на отчисление: {avt_faculty.get_students_for_expulsion()}')
+# print(f'Студенты-стипендиаты: {avt_faculty.get_scholarship_students()}')
+# print(f'Средняя оценка по предметам на факультете: {avt_faculty.get_subject_statistics()}')
 
