19b1a248b8f77d35919b1b2c921fbe6142be8b49
Vimpleks <evgenya_2005@mail.ru>
2026-03-15 21:43:48 +0700

    Add tasks module 9.1. 15.03.2026

---
+++ Tasks_Module_91.py
@@ -0,0 +1,58 @@
+# Задача 1.
+# Описание работы:
+# 1. Программа запрашивает у пользователя текст.
+# 2. Каждая новая фраза должна записываться в файл notes.txt с новой строки, не удаляя старые данные.
+# 3. Перед каждой фразой автоматически добавляется текущая дата и время (модуль datetime).
+# 4. Цикл завершается, когда пользователь вводит слово "стоп".
+# from datetime import datetime
+# datetime_note = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+# with open('notes.txt', mode= 'a') as notes_text:
+#     while True:
+#         text = input('Введите вашу заметку: ')
+#         if text.lower() == 'стоп':
+#             break
+#         notes_text.write(f'{datetime_note}\n')
+#         notes_text.write(f'{text}\n')
+
+# Задача 2
+# Сценарий работы:
+# 1. Пользователь вводит название файла (обязательно с расширением .csv и разделителем ';').
+# 2. Программа выводит названия столбцов, которые были найдены в файле.
+# 3. Пользователь выбирает некоторый столбец.
+# 4. Программа выводит список уникальных значений из выбранного столбца.
+# 5. Пользователь вводит выбранное для фильтрации значение.
+# 6. Программа записывает новый файл (в формате .csv) со строками, где выбранный столбец содержит указанное пользователем значение.
+
+import csv
+# user_file = input("Укажите название файла с расширением .csv: ")
+user_file = 'users.csv'
+with open(user_file, encoding="utf-8") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
+    names_column = {}
+    print("В файле найдены столбцы:")
+    for index, name in enumerate(next(csv_reader), start=1):
+        print(f"{index}. {name}")
+        names_column[index] = name
+user_column = int(input(f"Укажите столбец для фильтрации (введите цифру 1-{len(names_column)}): "))
+with open(user_file, encoding="utf-8") as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=';')
+    unique_column_values = set()
+    print(f"Уникальные значения столбца {user_column}:")
+    for row in csv_reader:
+        unique_column_values.add(row[names_column[user_column]])
+    unique_column_values_dict = {}
+    for index, element in enumerate(unique_column_values, start=1):
+        print(f"{index}. {element}")
+        unique_column_values_dict[index] = element
+value_filter = int(input(f"Укажите значение для фильтрации (введите цифру 1-{len(unique_column_values_dict)}): "))
+with open(user_file, encoding="utf-8") as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=';')
+    rows_filter = []
+    for row in csv_reader:
+        if row[names_column[user_column]] == unique_column_values_dict[value_filter]:
+            rows_filter.append(row)
+filter_file = f"users-{names_column[user_column]}-{unique_column_values_dict[value_filter]}.csv"
+with open(filter_file, "a", encoding="utf-8") as csv_file:
+    csv_writer = csv.DictWriter(csv_file, delimiter=';', fieldnames=('Никнейм','Имя','Фамилия','Город','Рейтинг'))
+    csv_writer.writeheader()
+    csv_writer.writerows(rows_filter)
