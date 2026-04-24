37126b3dce87403ecb5905f60c9046ee075e2e2b
Vimpleks <udacha.ev@gmail.com>
2026-04-24 16:56:25 +0700

    Add task module 11

--- Tasks_Module_9/Task_2_Module_9.py
+++ Tasks_Module_9/Task_2_Module_9.py
@@ -8,9 +8,10 @@
 # 6. Программа записывает новый файл (в формате .csv) со строками, где выбранный столбец содержит указанное пользователем значение.
 
 import csv
+my_delimiter = ','
 user_file = input("Укажите название файла с расширением .csv: ")
 with open(user_file, encoding="utf-8") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=';')
+    csv_reader = csv.reader(csv_file, delimiter=my_delimiter)
     names_column = {}
     print("В файле найдены столбцы:")
     for index, name in enumerate(next(csv_reader), start=1):
@@ -18,7 +19,7 @@ with open(user_file, encoding="utf-8") as csv_file:
         names_column[index] = name
 user_column = int(input(f"Укажите столбец для фильтрации (введите цифру 1-{len(names_column)}): "))
 with open(user_file, encoding="utf-8") as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=';')
+    csv_reader = csv.DictReader(csv_file, delimiter=my_delimiter)
     unique_column_values = set()
     print(f"Уникальные значения столбца {user_column}:")
     for row in csv_reader:
@@ -29,13 +30,13 @@ with open(user_file, encoding="utf-8") as csv_file:
         unique_column_values_dict[index] = element
 value_filter = int(input(f"Укажите значение для фильтрации (введите цифру 1-{len(unique_column_values_dict)}): "))
 with open(user_file, encoding="utf-8") as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=';')
+    csv_reader = csv.DictReader(csv_file, delimiter=my_delimiter)
     rows_filter = []
     for row in csv_reader:
         if row[names_column[user_column]] == unique_column_values_dict[value_filter]:
             rows_filter.append(row)
 filter_file = f"{user_file.rstrip('.csv')}-{names_column[user_column]}-{unique_column_values_dict[value_filter]}.csv"
 with open(filter_file, "w", encoding="utf-8", newline='') as csv_file:
-    csv_writer = csv.DictWriter(csv_file, delimiter=';', fieldnames=(list(names_column.values())))
+    csv_writer = csv.DictWriter(csv_file, delimiter=my_delimiter, fieldnames=(list(names_column.values())))
     csv_writer.writeheader()
     csv_writer.writerows(rows_filter)
